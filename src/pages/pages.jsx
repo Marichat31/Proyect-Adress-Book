af1bd0846e49e6d9dfe8d05f34d416bbd8625e02
Marichat31 <mariajuarezmatias0@gmail.com>
2025-06-16 08:37:53 -0600

    Cambio de nombre a archivo

--- src/pages/pages.jsx
+++ src/pages/pages.jsx
@@ -1,5 +1,5 @@
 export { default as Index } from "./Index.jsx";
 export { default as Contact } from "./contact.jsx";
 export { default as EditContact } from "./edit.jsx";
-export { default as Welcome } from "./welcome.jsx";
+export { default as Welcome } from "./Welcome.jsx";
 export { default as Root } from "./root.jsx";
